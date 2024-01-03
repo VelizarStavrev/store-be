30e3046061dfbb268bcd7471417fdba633b14276
Velizar Stavrev <velizar.g.stavrev@gmail.com>
2024-01-03 15:14:00 +0200

    Updating the field requirements for the user model.

--- src/models/User.ts
+++ src/models/User.ts
@@ -10,11 +10,26 @@ interface IUser {
 }
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  username: String,
-  password: String,
-  created: Number,
-  role: Number,
+  email: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: Number,
+    required: true,
+  },
 }, { optimisticConcurrency: true });
 
 const User = mongoose.model<IUser>('User', userSchema);
